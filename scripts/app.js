34955416d50d30ae213a1cc067bb8a9487866ff2
Ramis <ramismubick@gmail.com>
2022-04-05 15:13:13 +0300

    Add searching by genre in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
- var getTVprogramByDate = function(tabObjects, date) {
+ var getTVprogramByDate = function(tabObjects, date) { 
      console.log(date)
     var $main_tabs_cells = $(".main-tabs-cells");
     var $main_tabs_cell, $main_channel_box, $main_event_box_element;
@@ -24,11 +24,12 @@
         (channel.channel_events).forEach(function(channel_event) {
             //блок с временем и названием передачи
             $main_event_box_element = $("<div>").addClass("main-event-box-element");
+            $main_event_box_element.attr("genre", channel_event.tv_show_genre);
             //добавление времени передачи
             $main_event_time = $("<div>").addClass("main-event-time");
             $main_event_time.append($("<time>").text(channel_event.tv_show_time));
             $main_event_box_element.append($main_event_time);
-                //добавление названия передачи
+            //добавление названия передачи
             $main_event_name =  $("<div>").addClass("main-event-name");
             $main_event_name.append(($("<a>").attr("href", "#")).text(channel_event.tv_show_name));
             $main_event_box_element.append($main_event_name);
@@ -39,10 +40,25 @@
         $main_tabs_cells.append($main_tabs_cell);
     });
  };
+
+ var getShowByGenre = function() {
+    var $tv_show_selected_option;
+    $("#tv-show-select").change(function(){
+        $(".main-event-box-element").css({'background':'#01D8DD'})
+        $tv_show_selected_option = $("#tv-show-select option:selected");
+        $(".main-event-box-element").toArray().forEach(function(elem){
+            if($(elem).attr("genre") == $tv_show_selected_option.text()){
+                $(elem).css({'background': 'red'});
+            }
+        });
+    });
+ };
  
  var main = function (tabObjects) { 
     "use strict";
-    $(".main-tabs-items a span").toArray().forEach(function (element) { 
+    $("#tv-show-select").attr("selected", null);
+    $("#tv-show-select option:nth-child(1)").attr("selected", "selected");
+    $(".main-tabs-items a span").toArray().forEach(function (element) {
 	    $(element).on("click", function () {
 	        var $element = $(element);
 	        $(".main-tabs-items a span").removeClass("active");
@@ -58,11 +74,13 @@
 	        } 
             else if ($element.parent().is(":nth-child(13)")) { 
                 getTVprogramByDate(tabObjects, 2);
-	        } 
-	        return false;
+	        }
+            getShowByGenre();
+            return false;
 	    });
     })
     $(".main-tabs-items a span.today").trigger("click");
+    
 };
 
 $(document).ready(function () { 
